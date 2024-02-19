72ab2e649f42a9600763ef7f5cd9c16eb1395fec
AlaminHasanPro <alaminhasanpro@gmail.com>
2024-02-20 01:21:16 +0600

    corse and express totally connected

--- src/db/db.js
+++ src/db/db.js
@@ -6,7 +6,9 @@ const connectDB = async () => {
     const connectionInstant = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\MongoDB connected on !! DB HOST : ${process.env.PORT} ${connectionInstant.connection.host}`);
+    console.log(
+      `\MongoDB connected on !! DB HOST : ${connectionInstant.connection.host}`
+    );
   } catch (error) {
     console.log("MONGODB connection FAILED", error);
     process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000),
+      () => {
+        console.log(`Server is running on port : ${process.env.PORT}`);
+      };
+    app.on("error", (err) => {
+      console.log("ERR:", err);
+      throw err;
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed !!!", err);
+  });
 /*
 import express from "express";
 const app = express();
